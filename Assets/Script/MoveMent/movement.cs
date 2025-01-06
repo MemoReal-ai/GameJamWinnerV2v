ddb7067e241f2a2901003cbcd207814f4babbd22
p1rozok <com_djoker@list.ru>
2025-01-06 20:51:26 +0300

    heroATTACK

--- Assets/Script/MoveMent/movement.cs
+++ Assets/Script/MoveMent/movement.cs
@@ -1,32 +1,104 @@
+using System;
 using UnityEngine;
 
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
+
+    [SerializeField] private float constXY = 0f;
+    [SerializeField] private Animator animator;
     private float directionX;
     private float directionY;
     private Rigidbody2D rb ;
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         HarvestInput();
+        
     }
 
-    
+    private void Update()
+    { 
+        WalkAnimationController();
+    }
+
 
     private void HarvestInput()
     {
         directionX = Input.GetAxis("Horizontal");
         directionY = Input.GetAxis("Vertical");
-        var prefersMove = new Vector2(transform.position.x + directionX, transform.position.y + directionY);
+        var prefersMove = new Vector2(transform.position.x + directionX, transform.position.y + directionY) * speed;
         rb.position = prefersMove;
 
     }
+
+    private void WalkAnimationController()
+    {
+        if (directionX < 0)
+        {
+            SetAnimatorBools(walkLeft: true, walkRight: false, idle: false, walkDown: false, walkUp: false);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("AttackLeft");
+            }
+        }
+        else if (directionX > 0)
+        {
+            SetAnimatorBools(walkLeft: false, walkRight: true, idle: false, walkDown: false, walkUp: false);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("AttackRight");
+            }
+        }
+        else if (directionX == 0 && directionY == 0)
+        {
+            SetAnimatorBools(walkLeft: false, walkRight: false, idle: true, walkDown: false, walkUp: false);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("AttackLeft");
+            }
+        }
+
+        else if (directionY > 0)
+        {
+            SetAnimatorBools(walkLeft: false, walkRight: false, idle: false, walkUp: true, walkDown: false);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("AttackUp");
+            }
+        }
+        else if (directionY < 0)
+        {
+            SetAnimatorBools(walkLeft: false, walkRight: false, idle: false, walkUp: false, walkDown: true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("AttackDown");
+            }
+        }
+        
+    }
+
+
+    private void SetAnimatorBools(bool walkLeft, bool walkRight, bool idle, bool walkUp, bool walkDown)
+    {
+        animator.SetBool("walkLeft", walkLeft);
+        animator.SetBool("walkRight", walkRight);
+        animator.SetBool("idle", idle);
+        animator.SetBool("walkUp", walkUp);
+        animator.SetBool("walkDown", walkDown);
+        
+    }
+
     
-}
+        
+    }
+    
+    
+    
